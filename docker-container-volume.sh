937ddba4155ba51b887c33b916e32f5cccada9b0
reimiii <imiia75775@gmail.com>
2023-07-15 20:45:50 +0700

    docker-container-volume.sh

---
+++ docker-container-volume.sh
@@ -0,0 +1,5 @@
+docker volume create data-for-mongo
+
+docker container create --name mongovolume --publish 27019:27017 --mount "type=volume,source=data-for-mongo,destination=/data/db" --env MONGO_INITDB_ROOT_USERNAME=mee --env MONGO_INITDB_ROOT_PASSWORD=mee mongo:latest
+
+docker container stop mongovolume
